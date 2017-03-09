7c6a90ff5cd81101870308b65c850c456e44448d
lbreguet <louis-w@live.com>
2017-03-09 15:21:24 -0500

    Index.js created
    -after multiple rebasing issues and losing a very important file, we
     were able to get through the rebase issues and find the file

---
+++ assets/scripts/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const setAPIOrigin = require('../../lib/set-api-origin');
+const config = require('./config');
+
+$(() => {
+  setAPIOrigin(location, config);
+});
+
+// use require with a reference to bundle the file and use it in this file
+// const example = require('./example');
+
+// use require without a reference to ensure a file is bundled
+const auth = require('./auth/events');
+const bucklist = require('./bucketlists/events');
+const maps = require('./maps/events');
+const logic = require('./maps/map-logic');
+
+$(() => {
+  auth.addHandlers();
+  bucklist.addHandlers();
+  maps.addHandlers();
+});
