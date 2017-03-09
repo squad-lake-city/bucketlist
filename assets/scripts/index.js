a80a0321643b98adfbdd6e4aa7fedeaa4d9c00bc
cpstrillvt93 <cpsvt93@gmail.com>
2017-03-08 19:44:51 -0500

    Fix index.js bug
    Removed requirement of google-maps to facilitate testing.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,7 +13,7 @@ $(() => {
 // use require without a reference to ensure a file is bundled
 const auth = require('./auth/events');
 
-require('./maps/google-maps');
+// require('./maps/google-maps');
 
 $(() => {
   auth.addHandlers();
