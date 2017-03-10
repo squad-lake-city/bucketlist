f4e79f9790612594b3897019c2bd21344d009a27
cpstrillvt93 <cpsvt93@gmail.com>
2017-03-10 16:54:34 -0500

    Add UI messages to sign-up an sign-in

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,18 +2,22 @@
 
 const signInSuccess = function() {
   console.log("sign-in success");
+  $('.signin-failure').text('Signed in!');
 };
 
 const signInFailure = function() {
   console.log("sign-in failure");
+  $('.signin-failure').text('Failed sign-in attempt. User email may not exist and/or passwords may not match');
 };
 
 const signUpSuccess = function() {
   console.log("sign-up success");
+  $('.signup-failure').text('Signed up!');
 };
 
 const signUpFailure = function() {
   console.log("sign-up failure");
+  $('.signup-failure').text('Failed sign-up. The user email you used may be taken and/or your passwords match');
 };
 
 const changePasswordSuccess = function() {
