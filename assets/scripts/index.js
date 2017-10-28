74f8b199e1eb764199fd0c379b42122752f71ebe
Stephen Wisner <spwisner@gmail.com>
2017-10-28 16:53:30 -0400

    Included Show For Container Fluid on Page Load
    - Previously, the page showed content for a split second before hiding
    to display the homepage
    
    - This was fixed by using inline to hide the body content until the page
    is fully loaded (using jQuery $(".containerfluid").show();

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,6 +5,7 @@ const config = require('./config');
 
 $(() => {
   setAPIOrigin(location, config);
+  $(".container-fluid").show();
 });
 
 // use require with a reference to bundle the file and use it in this file
